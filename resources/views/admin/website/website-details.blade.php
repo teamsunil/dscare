602e3695ce8017a85ff110eae07c76d3ecb3fae1
Saurav kumar saraswat <saurav.saraswat@dotsquares.com>
2025-09-30 14:41:16 +0530

    update

--- resources/views/admin/website/website-details.blade.php
+++ resources/views/admin/website/website-details.blade.php
@@ -276,37 +276,6 @@ function loadDBOptData() {
 		error.textContent = 'No WordPress URL found.';
 		return;
 	}
-
-	// Reload website data button
-	const reloadBtn = document.getElementById('btn-reload-data');
-	if (reloadBtn) {
-		reloadBtn.addEventListener('click', function () {
-			reloadBtn.disabled = true;
-			reloadBtn.innerHTML = '<i class="fas fa-spinner fa-spin"></i> Reloading...';
-			const websiteId = @json($website->id ?? $id ?? null);
-			if (!websiteId) {
-				alert('Website ID not found.');
-				reloadBtn.disabled = false;
-				reloadBtn.innerHTML = 'Reload Data';
-				return;
-			}
-
-			fetch('/admin/website/reload/' + encodeURIComponent(websiteId) + '/data', { method: 'GET' })
-				.then(res => res.json().catch(() => ({})))
-				.then(data => {
-					// The controller returns redirects/back with flash messages; when called via fetch we expect JSON if successful.
-					// If fetch returns an empty object (because server responded with redirect/html), treat as success and reload.
-					// Reload the page to show updated data.
-					location.reload();
-				})
-				.catch(err => {
-					console.error('Reload data error', err);
-					alert('Failed to reload data. Check console for details.');
-					reloadBtn.disabled = false;
-					reloadBtn.innerHTML = 'Reload Data';
-				});
-		});
-	}
 	fetch(wpUrl.replace(/\/$/, '') + '/wp-json/laravel-sso/v1/db-optimize-data')
 		.then(res => res.json())
 		.then(data => {
